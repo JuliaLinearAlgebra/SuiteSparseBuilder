5923adce2d6c6d6fba5b7a546f14ae8ebeb2f247
ViralBShah <viral@mayin.org>
2018-05-09 19:56:01 -0400

    Working Linux 32-bit and 64-bit support on X86 and ARM.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -11,16 +11,31 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd SuiteSparse/
-make -j library BLAS="-L$prefix/lib -lopenblas64_" LAPACK="-L$prefix/lib -lopenblas64_" UMFPACK_CONFIG=-DSUN64 CHOLMOD_CONFIG=-DSUN64 SPQR_CONFIG=-DSUN64
-make -j install INSTALL=$prefix/lib BLAS="-L$prefix/lib -lopenblas64_" LAPACK="-L$prefix/lib -lopenblas64_" UMFPACK_CONFIG=-DSUN64 CHOLMOD_CONFIG=-DSUN64 SPQR_CONFIG=-DSUN64
+
+UNAME=`uname`
+if [[ ${UNAME} == MSYS_NT-6.3 ]]; then 
+     UNAME="Windows"
+fi
+
+if [[ ${nbits} == 64 ]]; then
+     make -j library UMFPACK_CONFIG="-DSUN64 -DLONGBLAS='long long'" CHOLMOD_CONFIG="-DSUN64 -DLONGBLAS='long long'" SPQR_CONFIG="-DSUN64 -DLONGBLAS='long long'"
+     make -j install INSTALL=$prefix/lib BLAS="-L$prefix/lib -lopenblas64_" LAPACK="-L$prefix/lib -lopenblas64_" UMFPACK_CONFIG="-DSUN64 -DLONGBLAS='long long'" CHOLMOD_CONFIG="-DSUN64 -DLONGBLAS='long long'" SPQR_CONFIG="-DSUN64 -DLONGBLAS='long long'"
+else
+     make -j library UNAME=${UNAME}
+     make -j install UNAME=${UNAME} INSTALL=$prefix/lib BLAS="-L$prefix/lib -lopenblas" LAPACK="-L$prefix/lib -lopenblas"
+fi
+
 
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
+#    BinaryProvider.Windows(:i686, :blank_libc, :blank_abi),
     BinaryProvider.Linux(:x86_64, :glibc, :blank_abi),
-    BinaryProvider.Linux(:aarch64, :glibc, :blank_abi)
+    BinaryProvider.Linux(:i686, :glibc, :blank_abi),
+    BinaryProvider.Linux(:aarch64, :glibc, :blank_abi),
+    BinaryProvider.Linux(:armv7l, :glibc, :eabihf)
 ]
 
 # The products that we will ensure are always built
